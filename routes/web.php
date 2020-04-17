6e77f6ca4c1de52ea5c744bbb34fe0d2cf2314fe
eduardo-soto1997 <50255822+eduardo-soto1997@users.noreply.github.com>
2020-04-17 17:34:20 -0500

    This is for you Angel Love you

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('/login', 'Pagecontroller@login');
 //Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
-/*
+
 Route::group(['middleware' => ['auth']], function () {
     //Route::get('/','PagesController@home');
     Route::get('/', 'HomeController@index')->name('home');
-*/
+  });
+
 Route::get('/schedule','courseController@index');
 
 Route::get('/', function () {
@@ -20,8 +21,6 @@ Route::get('/instructors', 'InstructorController@index')->name('instructors.inde
 //Route::get('/instructors','InstructorController@index');
 Route::get('/instructors/{id}/edit','InstructorController@edit')->name('instructors.edit');
 Route::get('/instructors/{id}/delete','InstructorController@destroy')->name('instructors.destroy');
-Route::get('/create','InstructorController@create')->name('instructors.create');
-Route::post('/create','InstructorController@store')->name('instructors.store');
+Route::get('/instructors/create','InstructorController@create')->name('instructors.create');
+Route::post('/instructors/create','InstructorController@store')->name('instructors.store');
 Route::post('/instructor/update','InstructorController@update')->name('instructors.update');
-// });
-
