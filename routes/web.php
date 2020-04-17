dfc178f9459c1d4d808f01ebb1a7bcbce742b345
eduardo-soto1997 <50255822+eduardo-soto1997@users.noreply.github.com>
2020-04-16 19:56:17 -0500

    Login and Auth

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', 'Pagecontroller@login');
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::group(['middleware' => ['auth']], function () {
+    //Route::get('/','PagesController@home');
+    Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/schedule','courseController@index');
+
+});
+
+
+Auth::routes();
