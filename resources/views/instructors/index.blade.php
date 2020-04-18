eb176597c29664f1f51800f7c482aea004640b0e
lmAlexander26 <galexanderrojas@gmail.com>
2020-04-18 18:17:17 -0500

    tiny changes

--- resources/views/instructors/index.blade.php
+++ resources/views/instructors/index.blade.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<table class="table">
-				<tr>
+				<tr class = "text-center">
 					<th>ID</th>
 					<th>Name</th>
 				</tr>
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/instructors', 'InstructorController@index')->name('instructors.index');
-//Route::get('/instructors','InstructorController@index');
 Route::get('/instructors/{id}/edit','InstructorController@edit')->name('instructors.edit');
 Route::get('/instructors/{id}/delete','InstructorController@destroy')->name('instructors.destroy');
 Route::get('/instructors/create','InstructorController@create')->name('instructors.create');
