7ef8796e66403333c7336141c517aece831bf087
eduardo-soto1997 <50255822+eduardo-soto1997@users.noreply.github.com>
2020-04-23 11:50:18 -0500

    auth fixes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Route::group(['middleware' => ['auth']], function () {
     //Route::get('/','PagesController@home');
-    Route::get('/', function(){
+Route::get('/', function(){
       return view('index');
     });
 
